2f8e5818122de2bd9e80e3cdccd92be4138b7dd8
Kunal Nawariya <146311711+NawariyaKunal262004@users.noreply.github.com>
2025-12-27 23:26:34 +0530

    Add api health check

--- backend/routes/test.routes.js
+++ backend/routes/test.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+console.log("TEST ROUTES FILE LOADED");
 const testController = require("../controllers/test.controller");
 
 router.get("/test", testController.test);
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
 require("dotenv").config();
+console.log("SERVER FILE LOADED");
 const express = require("express");
 const cors = require("cors");
-
-
-const testRoutes = require("./routes/test.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors()); // INTENTIONALLY SIMPLE FOR NOW
-console.log("About to load test routes");
-
-app.use("/api", testRoutes);
-
-
-const PORT =  process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Backend root working");
 });
+
+
+const testRoutes = require("./routes/test.routes");
+
+// console.log("About to load test routes");
+
+app.use("/api", testRoutes);
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "API alive" });
+});
+
+const PORT =  process.env.PORT || 5000;
 
 
 app.listen(PORT, () => {
