ed47dcdd2c9a300b5562d98975d815466105ebc0
Kunal Nawariya <146311711+NawariyaKunal262004@users.noreply.github.com>
2025-12-27 23:13:45 +0530

    Add route require log

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,17 @@ const app = express();
 
 app.use(express.json());
 app.use(cors()); // INTENTIONALLY SIMPLE FOR NOW
+console.log("About to load test routes");
 
 app.use("/api", testRoutes);
 
 
 const PORT =  process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend root working");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
